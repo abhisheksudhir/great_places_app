9713abb7634a573050591c9f6359901b4835e828
ABHISHEK SUDHIR <abhisheksudhir2000@gmail.com>
2020-06-14 20:15:36 +0530

    adding the add place screen & an image input

--- lib/screens/add_place_screen.dart
+++ lib/screens/add_place_screen.dart
@@ -1,1 +1,56 @@
 import 'package:flutter/material.dart';
+
+import '../widgets/image_input.dart';
+
+class AddPlaceScreen extends StatefulWidget {
+  static const routeName = '/add-place';
+
+  @override
+  _AddPlaceScreenState createState() => _AddPlaceScreenState();
+}
+
+class _AddPlaceScreenState extends State<AddPlaceScreen> {
+  final _titleController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Add a New Place',
+        ),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Expanded(
+            child: SingleChildScrollView(
+              child: Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Column(
+                  children: <Widget>[
+                    TextField(
+                      decoration: InputDecoration(labelText: 'Title'),
+                      controller: _titleController,
+                    ),
+                    SizedBox(height: 10),
+                    ImageInput(),
+                  ],
+                ),
+              ),
+            ),
+          ),
+          RaisedButton.icon(
+            icon: Icon(Icons.add),
+            label: Text('Add Place'),
+            elevation: 0,
+            materialTapTargetSize: MaterialTapTargetSize
+                .shrinkWrap, //to get rid of extra margin around the button
+            color: Theme.of(context).accentColor,
+            onPressed: () {},
+          ),
+        ],
+      ),
+    );
+  }
+}
